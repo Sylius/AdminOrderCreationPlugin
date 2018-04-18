e17fe76254e8b3574170d97aac98cd09035dac96
Mateusz Zalewski <zaleslaw@gmail.com>
2018-04-18 11:15:12 +0200

    Simple select new order customer form

--- src/Controller/OrderCreationController.php
+++ src/Controller/OrderCreationController.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Sylius\AdminOrderCreationPlugin\Controller;
 
+use Sylius\AdminOrderCreationPlugin\Form\Type\NewOrderCustomerSelectType;
 use Sylius\Bundle\CoreBundle\Controller\OrderController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -12,6 +13,10 @@ final class OrderCreationController extends OrderController
 {
     public function selectNewOrderCustomerAction(Request $request): Response
     {
-        return $this->render('@SyliusAdminOrderCreationPlugin/orderSelectCustomer.html.twig');
+        $selectCustomerForm = $this->get('form.factory')->create(NewOrderCustomerSelectType::class);
+
+        return $this->render('@SyliusAdminOrderCreationPlugin/orderSelectCustomer.html.twig', [
+            'form' => $selectCustomerForm->createView(),
+        ]);
     }
 }
