b6692e2bd77666f764a662ba27b5f81853274210
Mateusz Zalewski <zaleslaw@gmail.com>
2018-04-24 14:01:05 +0200

    Reactivate behat build

--- src/Factory/OrderFactory.php
+++ src/Factory/OrderFactory.php
@@ -5,6 +5,8 @@ namespace Sylius\AdminOrderCreationPlugin\Factory;
 use Sylius\Component\Channel\Repository\ChannelRepositoryInterface;
 use Sylius\Component\Core\Model\OrderInterface;
 use Sylius\Component\Core\Repository\CustomerRepositoryInterface;
+use Sylius\Component\Currency\Model\CurrencyInterface;
+use Sylius\Component\Locale\Model\LocaleInterface;
 use Sylius\Component\Resource\Factory\FactoryInterface;
 use Sylius\Component\Resource\Repository\RepositoryInterface;
 use Webmozart\Assert\Assert;
@@ -42,20 +44,32 @@ final class OrderFactory implements OrderFactoryInterface
 
     public function createNew(): OrderInterface
     {
-        return $this->decoratedFactory->createNew();
+        $order = $this->decoratedFactory->createNew();
+        assert($order instanceof OrderInterface);
+
+        return $order;
     }
 
     public function createForCustomer(string $customerEmail): OrderInterface
     {
         $customer = $this->customerRepository->findOneBy(['email' => $customerEmail]);
+
         Assert::notNull($customer);
 
         /** @var OrderInterface $order */
         $order = $this->decoratedFactory->createNew();
+        assert($order instanceof OrderInterface);
+
         $order->setCustomer($customer);
         $order->setChannel($this->channelRepository->findOneBy(['enabled' => true]));
-        $order->setCurrencyCode($this->currencyRepository->findOneBy([])->getCode());
-        $order->setLocaleCode($this->localeRepository->findOneBy([])->getCode());
+
+        $currency = $this->currencyRepository->findOneBy([]);
+        assert($currency instanceof CurrencyInterface);
+        $order->setCurrencyCode($currency->getCode());
+
+        $locale = $this->localeRepository->findOneBy([]);
+        assert($locale instanceof LocaleInterface);
+        $order->setLocaleCode($locale->getCode());
 
         return $order;
     }
