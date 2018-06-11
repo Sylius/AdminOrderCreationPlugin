5e24b173af490499ae48f0f96a7992f4ee9886fb
Mateusz Zalewski <zaleslaw@gmail.com>
2018-06-11 14:32:45 +0200

    Change customer select box to autocomplete dropdown

---
+++ src/Form/Type/CustomerAutocompleteChoiceType.php
@@ -0,0 +1,52 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Sylius\AdminOrderCreationPlugin\Form\Type;
+
+use Sylius\Bundle\ResourceBundle\Form\Type\ResourceAutocompleteChoiceType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormInterface;
+use Symfony\Component\Form\FormView;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+final class CustomerAutocompleteChoiceType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'resource' => 'sylius.customer',
+            'choice_name' => 'email',
+            'choice_value' => 'email',
+            'label' => false,
+        ]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function buildView(FormView $view, FormInterface $form, array $options): void
+    {
+        $view->vars['remote_criteria_type'] = 'contains';
+        $view->vars['remote_criteria_name'] = 'phrase';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix(): string
+    {
+        return 'sylius_customer_autocomplete_choice';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getParent(): string
+    {
+        return ResourceAutocompleteChoiceType::class;
+    }
+}
