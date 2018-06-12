b3cd18b74bcd1357b4214dc9c8073b7cd6dd62ec
Mateusz Zalewski <zaleslaw@gmail.com>
2018-06-12 08:43:43 +0200

    Autocomplete for variants in order items

---
+++ src/Doctrine/ORM/ProductVariantRepository.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Sylius\AdminOrderCreationPlugin\Doctrine\ORM;
+
+use Sylius\Bundle\CoreBundle\Doctrine\ORM\ProductVariantRepository as BaseProductVariantRepository;
+
+final class ProductVariantRepository extends BaseProductVariantRepository
+{
+    public function findByPhrase(string $phrase, string $locale): array
+    {
+        $expr = $this->getEntityManager()->getExpressionBuilder();
+
+        return $this->createQueryBuilder('o')
+            ->innerJoin('o.translations', 'translation', 'WITH', 'translation.locale = :locale')
+            ->andWhere($expr->orX('translation.name LIKE :phrase', 'o.code LIKE :phrase'))
+            ->setParameter('phrase', '%' . $phrase . '%')
+            ->setParameter('locale', $locale)
+            ->getQuery()
+            ->getResult()
+        ;
+    }
+}
