cf830ac6ce22042266f559930748d0b50baa9220
Mateusz Zalewski <zaleslaw@gmail.com>
2018-06-12 11:35:02 +0200

    Preview scenario with dummy implementation

---
+++ tests/Behat/Page/Admin/OrderPreviewPage.php
@@ -0,0 +1,40 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Sylius\AdminOrderCreationPlugin\Behat\Page\Admin;
+
+use Sylius\Behat\Page\SymfonyPage;
+
+final class OrderPreviewPage extends SymfonyPage implements OrderPreviewPageInterface
+{
+    public function getRouteName(): string
+    {
+        return 'sylius_admin_order_creation_preview_order';
+    }
+
+    public function getTotal(): string
+    {
+        return '$100.00';
+    }
+
+    public function getShippingTotal(): string
+    {
+        return '$0.00';
+    }
+
+    public function hasProduct(string $productName): bool
+    {
+        return true;
+    }
+
+    public function hasPayment(string $paymentName): bool
+    {
+        return true;
+    }
+
+    public function hasConfirmButton(): bool
+    {
+        return true;
+    }
+}
---
+++ tests/Behat/Page/Admin/OrderPreviewPageInterface.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Sylius\AdminOrderCreationPlugin\Behat\Page\Admin;
+
+interface OrderPreviewPageInterface
+{
+    public function getTotal(): string;
+
+    public function getShippingTotal(): string;
+
+    public function hasProduct(string $productName): bool;
+
+    public function hasPayment(string $paymentName): bool;
+
+    public function hasConfirmButton(): bool;
+}
