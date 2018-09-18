712341ea2b1c0601a4786294d64d0094cf04c91d
mamazu <maxo.pech@gmail.com>
2018-09-18 15:42:49 +0200

    Fixed infinite spinner for items that do not require shipping

--- src/Controller/ProvideAvailableShippingMethodsAction.php
+++ src/Controller/ProvideAvailableShippingMethodsAction.php
@@ -6,6 +6,7 @@ namespace Sylius\AdminOrderCreationPlugin\Controller;
 
 use Sylius\AdminOrderCreationPlugin\Preparator\OrderPreparatorInterface;
 use Sylius\AdminOrderCreationPlugin\Provider\AvailableShippingMethodsListProvider;
+use Sylius\Component\Core\Model\ShipmentInterface;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -31,6 +32,11 @@ final class ProvideAvailableShippingMethodsAction
         $order = $this->orderPreparator->prepareFromRequest($request);
         $shipment = $order->getShipments()->get((int) $request->attributes->get('shipmentNumber'));
 
-        return new JsonResponse($this->availableShippingMethodsListProvider->__invoke($shipment));
+        $jsonResponse = [];
+        if ($shipment instanceof ShipmentInterface) {
+            $jsonResponse = $this->availableShippingMethodsListProvider->__invoke($shipment);
+        }
+
+        return new JsonResponse($jsonResponse, 200);
     }
 }
