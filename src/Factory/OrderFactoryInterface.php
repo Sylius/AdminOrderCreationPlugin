58656bc2afead69f31d1a0b31ca74e2ec1fb04d9
mamazu <maxo.pech@gmail.com>
2018-09-24 09:23:56 +0200

    Fixed types and added documentation

--- src/Factory/OrderFactoryInterface.php
+++ src/Factory/OrderFactoryInterface.php
@@ -9,7 +9,18 @@ use Sylius\Component\Resource\Factory\FactoryInterface;
 
 interface OrderFactoryInterface extends FactoryInterface
 {
-    public function createForCustomerAndChannel(string $customerEmail, string $channelCode): OrderInterface;
+    /**
+     * Creates a new order for a given customer in a given channel.
+     * 
+     * @param string $customerId
+     * @param string $channelCode
+     */
+    public function createForCustomerAndChannel(string $customerId, string $channelCode): OrderInterface;
 
+    /**
+     * Creates an order from an existing order and runs the reoder logic
+     * 
+     * @param OrderInterface $order
+     */
     public function createFromExistingOrder(OrderInterface $order): OrderInterface;
 }
