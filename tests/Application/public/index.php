6e990bd3e0b798dc973183e36cda226e8077c413
mati <mateusz.koruna@digitalholding.pl>
2021-03-24 09:12:06 +0100

    Next iteration

--- tests/Application/public/index.php
+++ tests/Application/public/index.php
@@ -1,10 +1,10 @@
 <?php
 
 use Tests\Sylius\AdminOrderCreationPlugin\Application\Kernel;
-use Symfony\Component\Debug\Debug;
+use Symfony\Component\ErrorHandler\Debug;
 use Symfony\Component\HttpFoundation\Request;
 
-require dirname(__DIR__).'/config/bootstrap.php';
+require dirname(__DIR__) . '/config/bootstrap.php';
 
 if ($_SERVER['APP_DEBUG']) {
     umask(0000);
