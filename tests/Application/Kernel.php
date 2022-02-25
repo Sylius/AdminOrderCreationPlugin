7796e5f72803020e3ae257406203ce82f5d840fa
Grzegorz Sadowski <sadowskigp@gmail.com>
2022-02-25 10:45:06 +0100

    [Maintenance] Drop support for Sylius 1.9 + add for Sylius 1.11

--- tests/Application/Kernel.php
+++ tests/Application/Kernel.php
@@ -5,10 +5,12 @@ declare(strict_types=1);
 namespace Tests\Sylius\AdminOrderCreationPlugin\Application;
 
 use PSS\SymfonyMockerContainer\DependencyInjection\MockerContainer;
+use Sylius\Bundle\CoreBundle\Application\Kernel as SyliusKernel;
 use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
 use Symfony\Component\Config\Loader\LoaderInterface;
 use Symfony\Component\Config\Resource\FileResource;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\HttpKernel\Bundle\BundleInterface;
 use Symfony\Component\HttpKernel\Kernel as BaseKernel;
 use Symfony\Component\Routing\RouteCollectionBuilder;
 
@@ -17,6 +19,13 @@ final class Kernel extends BaseKernel
     use MicroKernelTrait;
 
     private const CONFIG_EXTS = '.{php,xml,yaml,yml}';
+
+    private const IGNORED_SERVICES_DURING_CLEANUP = [
+        'kernel',
+        'http_kernel',
+        'liip_imagine.mime_type_guesser',
+        'liip_imagine.extension_guesser',
+    ];
 
     public function getCacheDir(): string
     {
@@ -30,7 +39,74 @@ final class Kernel extends BaseKernel
 
     public function registerBundles(): iterable
     {
-        $contents = require $this->getProjectDir() . '/config/bundles.php';
+        foreach ($this->getBundleListFiles() as $file) {
+            yield from $this->registerBundlesFromFile($file);
+        }
+    }
+
+    protected function configureContainer(ContainerBuilder $container, LoaderInterface $loader): void
+    {
+        foreach ($this->getBundleListFiles() as $file) {
+            $container->addResource(new FileResource($file));
+        }
+
+        $container->setParameter('container.dumper.inline_class_loader', true);
+
+        foreach ($this->getConfigurationDirectories() as $confDir) {
+            $this->loadContainerConfiguration($loader, $confDir);
+        }
+    }
+
+    protected function configureRoutes(RouteCollectionBuilder $routes): void
+    {
+        foreach ($this->getConfigurationDirectories() as $confDir) {
+            $this->loadRoutesConfiguration($routes, $confDir);
+        }
+    }
+
+    protected function getContainerBaseClass(): string
+    {
+        if ($this->isTestEnvironment() && class_exists(MockerContainer::class)) {
+            return MockerContainer::class;
+        }
+
+        return parent::getContainerBaseClass();
+    }
+
+    private function isTestEnvironment(): bool
+    {
+        return 0 === strpos($this->getEnvironment(), 'test');
+    }
+
+    private function loadContainerConfiguration(LoaderInterface $loader, string $confDir): void
+    {
+        $loader->load($confDir . '/{packages}/*' . self::CONFIG_EXTS, 'glob');
+        $loader->load($confDir . '/{packages}/' . $this->environment . '/**/*' . self::CONFIG_EXTS, 'glob');
+        $loader->load($confDir . '/{services}' . self::CONFIG_EXTS, 'glob');
+        $loader->load($confDir . '/{services}_' . $this->environment . self::CONFIG_EXTS, 'glob');
+    }
+
+    private function loadRoutesConfiguration(RouteCollectionBuilder $routes, string $confDir): void
+    {
+        $routes->import($confDir . '/{routes}/*' . self::CONFIG_EXTS, '/', 'glob');
+        $routes->import($confDir . '/{routes}/' . $this->environment . '/**/*' . self::CONFIG_EXTS, '/', 'glob');
+        $routes->import($confDir . '/{routes}' . self::CONFIG_EXTS, '/', 'glob');
+    }
+
+    /**
+     * @return BundleInterface[]
+     */
+    private function registerBundlesFromFile(string $bundlesFile): iterable
+    {
+        $contents = require $bundlesFile;
+
+        if (SyliusKernel::MINOR_VERSION > 10) {
+            $contents = array_merge(
+                ['Sylius\Calendar\SyliusCalendarBundle' => ['all' => true]],
+                $contents
+            );
+        }
+
         foreach ($contents as $class => $envs) {
             if (isset($envs['all']) || isset($envs[$this->environment])) {
                 yield new $class();
@@ -38,35 +114,32 @@ final class Kernel extends BaseKernel
         }
     }
 
-    protected function configureContainer(ContainerBuilder $container, LoaderInterface $loader): void
+    /**
+     * @return string[]
+     */
+    private function getBundleListFiles(): array
     {
-        $container->addResource(new FileResource($this->getProjectDir() . '/config/bundles.php'));
-        $container->setParameter('container.dumper.inline_class_loader', true);
-        $confDir = $this->getProjectDir() . '/config';
-        $loader->load($confDir . '/{packages}/*' . self::CONFIG_EXTS, 'glob');
-        $loader->load($confDir . '/{packages}/' . $this->environment . '/**/*' . self::CONFIG_EXTS, 'glob');
-        $loader->load($confDir . '/{services}' . self::CONFIG_EXTS, 'glob');
-        $loader->load($confDir . '/{services}_' . $this->environment . self::CONFIG_EXTS, 'glob');
+        return array_filter(
+            array_map(
+                static function (string $directory): string {
+                    return $directory . '/bundles.php';
+                },
+                $this->getConfigurationDirectories()
+            ),
+            'file_exists'
+        );
     }
 
-    protected function configureRoutes(RouteCollectionBuilder $routes): void
+    /**
+     * @return string[]
+     */
+    private function getConfigurationDirectories(): array
     {
-        $confDir = $this->getProjectDir() . '/config';
-        $routes->import($confDir . '/{routes}/*' . self::CONFIG_EXTS, '/', 'glob');
-        $routes->import($confDir . '/{routes}/' . $this->environment . '/**/*' . self::CONFIG_EXTS, '/', 'glob');
-        $routes->import($confDir . '/{routes}' . self::CONFIG_EXTS, '/', 'glob');
-    }
+        $directories = [
+            $this->getProjectDir() . '/config',
+            $this->getProjectDir() . '/config/sylius/' . SyliusKernel::MAJOR_VERSION . '.' . SyliusKernel::MINOR_VERSION,
+        ];
 
-    protected function getContainerBaseClass(): string
-    {
-        if ($this->isTestEnvironment()) {
-            return MockerContainer::class;
-        }
-        return parent::getContainerBaseClass();
-    }
-
-    private function isTestEnvironment(): bool
-    {
-        return 0 === strpos($this->getEnvironment(), 'test');
+        return array_filter($directories, 'file_exists');
     }
 }
