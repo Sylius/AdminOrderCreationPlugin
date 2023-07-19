b2eb3ec2abb41c7eac6f607868b7434984b644d4
Florent Duthoit <florent.duthoit@fm2d.com>
2023-07-19 14:08:27 +0700

    Remove unused js import

--- src/Resources/assets/admin/js/index.js
+++ src/Resources/assets/admin/js/index.js
@@ -1,7 +1,4 @@
-import 'semantic-ui-css/components/dropdown';
-import 'semantic-ui-css/components/transition';
-
-import OrderCreateAddressSelect from './OrderCreateAddressSelect';
+ import OrderCreateAddressSelect from './OrderCreateAddressSelect';
 
 $(function() {
     OrderCreateAddressSelect();
